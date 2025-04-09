46711b835b02960d2be97c494abd207205737436
Sergey Bogomolov <bogomolovs693@gmail.com>
2025-04-09 08:54:14 +0300

    update task proto and add created_at

--- Lessons/internal/domain/task.go
+++ Lessons/internal/domain/task.go
@@ -1,8 +1,11 @@
 package domain
 
+import "time"
+
 type Task struct {
-	ID       string // Уникальный идентификатор задания
-	CourseID string // Идентификатор курса, к которому относится задание
-	Title    string // Название задания
-	Content  string // Содержание задания
+	ID        string    // Уникальный идентификатор задания
+	CourseID  string    // Идентификатор курса, к которому относится задание
+	Title     string    // Название задания
+	Content   string    // Содержание задания
+	CreatedAt time.Time // Дата создания задания
 }
--- Lessons/internal/repo/models.go
+++ Lessons/internal/repo/models.go
@@ -1,19 +1,24 @@
 package repo
 
-import "Classroom/Lessons/internal/domain"
+import (
+	"Classroom/Lessons/internal/domain"
+	"time"
+)
 
 type Task struct {
-	ID       string `db:"task_id"`
-	CourseID string `db:"course_id"`
-	Title    string `db:"title"`
-	Content  string `db:"content"`
+	ID        string    `db:"task_id"`
+	CourseID  string    `db:"course_id"`
+	Title     string    `db:"title"`
+	Content   string    `db:"content"`
+	CreatedAt time.Time `db:"created_at"`
 }
 
 func (t Task) ToEntity() domain.Task {
 	return domain.Task{
-		ID:       t.ID,
-		CourseID: t.CourseID,
-		Title:    t.Title,
-		Content:  t.Content,
+		ID:        t.ID,
+		CourseID:  t.CourseID,
+		Title:     t.Title,
+		Content:   t.Content,
+		CreatedAt: t.CreatedAt,
 	}
 }
