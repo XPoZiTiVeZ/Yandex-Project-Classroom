2b7c7142b5435002cbf352b195c1ec1bcd4058b4
Sergey Bogomolov <bogomolovs693@gmail.com>
2025-04-11 19:11:57 +0300

    add lessons crud

--- Lessons/internal/repo/models.go
+++ Lessons/internal/repo/models.go
@@ -36,3 +36,21 @@ func (t TaskStatus) ToEntity() domain.TaskStatus {
 		IsCompleted: t.IsCompleted,
 	}
 }
+
+type Lesson struct {
+	ID          string    `db:"lesson_id"`
+	CourseID    string    `db:"course_id"`
+	Title       string    `db:"title"`
+	Description string    `db:"description"`
+	CreatedAt   time.Time `db:"created_at"`
+}
+
+func (l Lesson) ToEntity() domain.Lesson {
+	return domain.Lesson{
+		ID:          l.ID,
+		CourseID:    l.CourseID,
+		Title:       l.Title,
+		Description: l.Description,
+		CreatedAt:   l.CreatedAt,
+	}
+}
