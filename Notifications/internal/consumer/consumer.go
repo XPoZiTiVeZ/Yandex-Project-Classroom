ec3758034fad316c5899446870810d89e892fe88
Sergey Bogomolov <bogomolovs693@gmail.com>
2025-04-22 10:22:02 +0300

    add event handlers

--- Notifications/internal/consumer/consumer.go
+++ Notifications/internal/consumer/consumer.go
@@ -1,12 +1,15 @@
 package consumer
 
 import (
+	"Classroom/Notifications/pkg/events"
 	"Classroom/Notifications/pkg/logger"
 	"context"
 	"log"
 
 	"github.com/IBM/sarama"
 )
+
+type Service interface{}
 
 type consumer struct {
 	master sarama.Consumer
@@ -47,14 +50,43 @@ func (c *consumer) ConsumeTopic(ctx context.Context, topic string) {
 			for {
 				select {
 				case msg := <-pc.Messages():
-					logger.Info(ctx, "Получено сообщение", "topic", msg.Topic, "partition", msg.Partition, "offset", msg.Offset, "key", string(msg.Key), "value", string(msg.Value))
+					handler, ok := handlers[msg.Topic]
+					if !ok {
+						logger.Error(ctx, "unknown topic", "topic", msg.Topic)
+						continue
+					}
+					handler(ctx, msg)
 				case err := <-pc.Errors():
-					logger.Error(ctx, "Получена ошибка", "err", err)
+					logger.Error(ctx, "failed to consume message", "err", err)
 				case <-ctx.Done():
-					logger.Info(ctx, "Получен сигнал завершения.")
 					return
 				}
 			}
 		}(pc)
 	}
+}
+
+type EventHandler func(ctx context.Context, msg *sarama.ConsumerMessage)
+
+var handlers = map[string]EventHandler{
+	events.CourseEnrolledTopic: handleUserEnrolled,
+	events.CourseExpelledTopic: handleUserExpelled,
+	events.LessonCreatedTopic:  handleLessonCreated,
+	events.TaskCreatedTopic:    handleTaskCreated,
+}
+
+func handleUserEnrolled(ctx context.Context, msg *sarama.ConsumerMessage) {
+	logger.Info(ctx, "Получено сообщение", "topic", msg.Topic, "partition", msg.Partition, "offset", msg.Offset, "key", string(msg.Key), "value", string(msg.Value))
+}
+
+func handleUserExpelled(ctx context.Context, msg *sarama.ConsumerMessage) {
+	logger.Info(ctx, "Получено сообщение", "topic", msg.Topic, "partition", msg.Partition, "offset", msg.Offset, "key", string(msg.Key), "value", string(msg.Value))
+}
+
+func handleLessonCreated(ctx context.Context, msg *sarama.ConsumerMessage) {
+	logger.Info(ctx, "Получено сообщение", "topic", msg.Topic, "partition", msg.Partition, "offset", msg.Offset, "key", string(msg.Key), "value", string(msg.Value))
+}
+
+func handleTaskCreated(ctx context.Context, msg *sarama.ConsumerMessage) {
+	logger.Info(ctx, "Получено сообщение", "topic", msg.Topic, "partition", msg.Partition, "offset", msg.Offset, "key", string(msg.Key), "value", string(msg.Value))
 }
