da9cb323e96e313c654f17c9d6868acf0d34064b
Dmitriy Lukashev <lukashevda@inbox.ru>
2025-04-24 18:19:08 +0300

    gitlab-ci update

---
+++ Pipelines/cover/auth_cover.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# * Папки для исключения из покрытия
+EXCLUDE_DIRS=(
+  "mocks"
+  "pkg"
+  "cmd"
+)
+
+# * Папка с микросевисом 
+# ! Обязательно изменить на свой
+MICROSERVICE_NAME="Auth"
+
+cd "$MICROSERVICE_NAME"
+EXCLUDE_PATHS=$(IFS="|"; echo "${EXCLUDE_DIRS[*]}")
+PATHS_FOR_TESTS=$(go list ./... | grep -v -E "$EXCLUDE_PATHS")
+
+# * Тестирование
+# * Если нужна доп. информация, закомментируйте '> /dev/null 2>&1'
+go test -coverprofile=coverage.out $PATHS_FOR_TESTS > /dev/null 2>&1
+COVERAGE_PERCENT=$(go tool cover -func=coverage.out | grep "total:" | awk '{print int($3)}')
+rm coverage.out
+
+cd ..
+
+echo "$COVERAGE_PERCENT"
